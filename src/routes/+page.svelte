0568a650ccb45c33c5c526cadbbb3336d8763d2f
youdonthavetobeprettyanymore <youdonthavetobeprettyanymore@gmail.com>
2024-12-23 01:05:21 -0800

    add functioning grid col feature.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,34 +2,272 @@
 
 <p>Gangnam Style.</p>
 
-<div class="flex w-full flex-wrap justify-center gap-1">
-	<!-- Images -->
-	<img
-		src="/landingPageImages/430112845_827620606073526_2915429404031556355_n.jpg"
-		class="h-40 w-auto object-contain"
-		alt="1"
-	/>
-	<img
-		src="/landingPageImages/434720383_843288557840064_5511332457670117434_n.jpg"
-		class="h-40 w-auto object-contain"
-		alt="2"
-	/>
-	<img
-		src="/landingPageImages/440207686_857396416429278_6460285669341999779_n.jpg"
-		class="h-40 w-auto object-contain"
-		alt="3"
-	/>
-	<img
-		src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
-		class="h-40 w-auto object-contain"
-		alt="4"
-	/>
-	<img src="/landingPageImages/image%20(3).png" class="h-40 w-auto object-contain" alt="5" />
-	<img src="/landingPageImages/img_5057.jpg" class="h-40 w-auto object-contain" alt="6" />
-	<img src="/landingPageImages/img_5057.jpg" class="h-40 w-auto object-contain" alt="7" />
-	<img src="/landingPageImages/img_5057.jpg" class="h-40 w-auto object-contain" alt="8" />
-	<img src="/landingPageImages/img_5057.jpg" class="h-40 w-auto object-contain" alt="9" />
-	<img src="/landingPageImages/img_5057.jpg" class="h-40 w-auto object-contain" alt="10" />
-	<img src="/landingPageImages/img_5057.jpg" class="h-40 w-auto object-contain" alt="11" />
-	<img src="/landingPageImages/img_5057.jpg" class="h-40 w-auto object-contain" alt="12" />
-</div>
+<section class="flex h-full w-full items-center justify-center bg-black">
+	<div class="grid max-h-full w-full grid-cols-6 gap-0.5 sm:grid-cols-12">
+		<!-- Static Images -->
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 1"
+		>
+			<img
+				src="/landingPageImages/430112845_827620606073526_2915429404031556355_n.jpg"
+				alt="1"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 2"
+		>
+			<img
+				src="/landingPageImages/434720383_843288557840064_5511332457670117434_n.jpg"
+				alt="2"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 3"
+		>
+			<img
+				src="/landingPageImages/440207686_857396416429278_6460285669341999779_n.jpg"
+				alt="3"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 1"
+		>
+			<img
+				src="/landingPageImages/430112845_827620606073526_2915429404031556355_n.jpg"
+				alt="1"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 2"
+		>
+			<img
+				src="/landingPageImages/434720383_843288557840064_5511332457670117434_n.jpg"
+				alt="2"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 3"
+		>
+			<img
+				src="/landingPageImages/440207686_857396416429278_6460285669341999779_n.jpg"
+				alt="3"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+		<button
+			class="m-0 cursor-pointer border-none bg-black bg-transparent p-0"
+			aria-label="Open Image 4"
+		>
+			<img
+				src="/landingPageImages/457966934_943697604465825_6968636407756041023_n.jpg"
+				alt="4"
+				class="block h-full w-full object-cover"
+				loading="lazy"
+			/>
+		</button>
+	</div>
+</section>
